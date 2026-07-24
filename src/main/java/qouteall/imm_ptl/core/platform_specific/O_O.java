079e034808b62b637214cabd656f91aade8b14ce
paperclip3 <143960523+paperclip3@users.noreply.github.com>
2026-07-24 00:02:50 -0500

    fix dimension stack crash

--- src/main/java/qouteall/imm_ptl/core/platform_specific/O_O.java
+++ src/main/java/qouteall/imm_ptl/core/platform_specific/O_O.java
@@ -242,9 +242,11 @@ public class O_O {
     
     @Nullable
     public static String getModName(String modid) {
-        return FMLLoader.getLoadingModList().getModFileById(modid).getMods().stream().findFirst().get().getDisplayName();
+        if (ModList.get().getModFileById(modid) == null) {
+            return modid;
+        }
+        return ModList.get().getModFileById(modid).getMods().stream().findFirst().get().getDisplayName();
     }
-    
     // most quilt installations use quilted fabric api
     public static boolean isQuilt() {
         return false;
